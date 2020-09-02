a57851902f9e2fc7f678608181c931f520dc0345
NuxiiGit <nuxii09@gmail.com>
2020-09-02 01:15:33 +0100

    added json option to command line application

--- main.go
+++ main.go
@@ -9,22 +9,33 @@ import (
 
 func main() {
     args := os.Args[1 :]
-    if len(args) < 1 {
+    argCount := len(args)
+    if argCount < 1 {
         fmt.Println("usage:\n  go-scrape <url>")
     } else {
-        url := args[0]
+        var emitter string
+        var url string
+        if argCount == 1 {
+            emitter = ""
+            url = args[0]
+        } else {
+            emitter = args[0]
+            url = args[1]
+        }
         context, err := scraper.ReadFile(url)
         if err != nil {
             fmt.Printf("an error occurred when scraping:\n%s\n", err)
+        } else if emitter == "" {
+            fmt.Println(string(context))
         } else {
             node, err := scraper.DecodeHTML(context)
+            var buffer bytes.Buffer
             if err != nil {
                 fmt.Printf("an error occurred when decoding the page:\n%s\n", err)
-            } else {
-                var buffer bytes.Buffer
+            } else if emitter == "-json" {
                 node.EncodeJSON(&buffer)
-                fmt.Printf("%s\n", &buffer)
             }
+            fmt.Printf("%s\n", &buffer)
         }
     }
 }
