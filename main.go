810a5e28ae27d6076f5dac54e0fd8aaf22ba964b
NuxiiGit <nuxii09@gmail.com>
2020-09-03 01:59:16 +0100

    revamped the cli

--- main.go
+++ main.go
@@ -7,32 +7,39 @@ import (
     "bytes"
 )
 
+func printHelp() {
+    fmt.Println("usage:\n  go-scrape <url> [json|xml]")
+}
+
 func main() {
     args := os.Args[1 :]
     argCount := len(args)
     if argCount < 1 {
-        fmt.Println("usage:\n  go-scrape <url> [json|xml]")
+        printHelp()
     } else {
         url := args[0]
         context, err := scraper.ReadFile(url)
         if err != nil {
-            fmt.Printf("an error occurred when scraping:\n%s\n", err)
+            fmt.Printf("an error occurred when scraping!\n%s\n", err)
         } else if argCount == 1 {
             fmt.Println(string(context))
         } else {
             emitter := args[1]
             node, err := scraper.DecodeHTML(context)
-            var buffer bytes.Buffer
             if err != nil {
-                fmt.Printf("an error occurred when decoding the page:\n%s\n", err)
+                fmt.Printf("an error occurred when decoding the page!\n%s\n", err)
             } else if emitter == "json" {
+                var buffer bytes.Buffer
                 node.EncodeJSON(&buffer)
+                fmt.Print(&buffer)
             } else if emitter == "xml" {
+                var buffer bytes.Buffer
                 node.EncodeXML(&buffer)
+                fmt.Print(&buffer)
             } else {
-                fmt.Printf("unknown emitter: %s\n", emitter)
+                fmt.Printf("unknown renderer '%s'\n", emitter)
+                printHelp()
             }
-            fmt.Printf("%s\n", &buffer)
         }
     }
 }
