4647526895ac3efd3c1f73ffde99afdd8b18156f
NuxiiGit <nuxii09@gmail.com>
2020-09-03 01:54:21 +0100

    modifications to command line app

--- main.go
+++ main.go
@@ -11,30 +11,23 @@ func main() {
     args := os.Args[1 :]
     argCount := len(args)
     if argCount < 1 {
-        fmt.Println("usage:\n  go-scrape [-json|xml] <url>")
+        fmt.Println("usage:\n  go-scrape <url> [json|xml]")
     } else {
-        var emitter string
-        var url string
-        if argCount == 1 {
-            emitter = ""
-            url = args[0]
-        } else {
-            emitter = args[0]
-            url = args[1]
-        }
+        url := args[0]
         context, err := scraper.ReadFile(url)
         if err != nil {
             fmt.Printf("an error occurred when scraping:\n%s\n", err)
-        } else if emitter == "" {
+        } else if argCount == 1 {
             fmt.Println(string(context))
         } else {
+            emitter := args[1]
             node, err := scraper.DecodeHTML(context)
             var buffer bytes.Buffer
             if err != nil {
                 fmt.Printf("an error occurred when decoding the page:\n%s\n", err)
-            } else if emitter == "-json" {
+            } else if emitter == "json" {
                 node.EncodeJSON(&buffer)
-            } else if emitter == "-xml" {
+            } else if emitter == "xml" {
                 node.EncodeXML(&buffer)
             } else {
                 fmt.Printf("unknown emitter: %s\n", emitter)
